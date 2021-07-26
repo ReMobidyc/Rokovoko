e99b885fb5ed6bd3ca8adad81e8fe5c68517d8a6
Feddal Nordine <nordine.feddal.etu@univ-lille.fr>
2021-07-26 10:18:49 +0200

    ROK: REFACTOR renamed file to navBar.js and also migrate app.js navbar jsx code here

--- src/App.js
+++ src/App.js
@@ -1,40 +1,18 @@
 import React, { Component } from "react";
-import { Link, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Register from "./components/register";
 import ListingSimulation from "./components/listingSimulations"
-import logo from './images/logo.png'
+
 import FormNavBar from "./components/formNavBar";
 import ReadingSimulation from "./components/readSimulation";
+import Footer from "./components/footer";
 class App extends Component {
   render() {
-    return (<div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a href="/" className="navbar-brand">
-          <img src={logo} alt='reMobidyc logo' /> reMobidycServer
-        </a>
-        <div className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <Link to={"/register"} className="nav-link">
-              Register
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to={"/runs/:id"} className="nav-link">
-              Tokens
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to={"/runs"} className="nav-link">
-              Runs
-            </Link>
-          </li>
-          <FormNavBar/>
-        </div>
-
-      </nav>
-
-
+    return (<div className="d-flex flex-column min-vh-100">
+      <header>
+        <FormNavBar/>
+      </header>
       <div className="container mt-3">
         <Switch>
           <Route exact path="/register" component={Register} />
@@ -42,7 +20,7 @@ class App extends Component {
           <Route exact path='/runs/:id' component={ReadingSimulation}/>
         </Switch>
       </div>
-
+      <Footer/>
     </div>
 
     )
