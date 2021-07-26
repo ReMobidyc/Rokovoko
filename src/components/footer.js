8a9f97de207f1f306593e5115bbaeca570f83b83
Feddal Nordine <nordine.feddal.etu@univ-lille.fr>
2021-07-26 10:51:56 +0200

    ROK: FEAT added base for footer

---
+++ src/components/footer.js
@@ -0,0 +1,16 @@
+
+export default function Footer(props) {
+
+
+    return (
+
+        <footer className="mt-auto bg-dark text-white">
+            <div className="container">
+                <a href="/">ReMobidycServer</a>
+                <span> &copy; 2021 DGtalAquaLab</span>
+            </div>
+        </footer>
+
+    )
+
+}
