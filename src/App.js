874bd5ffb51a8c89f93f11ef9c78458b208a870b
Feddal Nordine <nordine.feddal.etu@univ-lille.fr>
2021-07-27 19:11:34 +0200

    ROK: REFACTOR reformat code and added indexPage component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 import React, { Component } from "react";
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Register from "./components/register";
-import ListingSimulation from "./components/listingSimulations"
-
-import NavBar from "./components/navBar"; 
+import ListingSimulation from "./components/listingSimulations";
+import IndexPage from "./components/indexPage";
+import NavBar from "./components/navBar";
 import ReadingSimulation from "./components/readSimulation";
 import Footer from "./components/footer";
 class App extends Component {
   render() {
-    return (<div className="d-flex flex-column min-vh-100">
-      <header>
-        <NavBar/>
-      </header>
-      <div className="container mt-3">
-        <Switch>
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/runs" component={ListingSimulation} />
-          <Route exact path='/runs/:id' component={ReadingSimulation}/>
-        </Switch>
+    return (
+      <div className="d-flex flex-column min-vh-100">
+        <header>
+          <NavBar />
+        </header>
+        <div className="container mt-3">
+          <Switch>
+            <Route exact path="/" component={IndexPage} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/runs" component={ListingSimulation} />
+            <Route exact path="/runs/:id" component={ReadingSimulation} />
+          </Switch>
+        </div>
+        <Footer />
       </div>
-      <Footer/>
-    </div>
-
-    )
-
+    );
   }
 }
 
