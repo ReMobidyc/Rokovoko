a3dd5ec2790cb1b9ef265f03d028758a63f5d1cf
Feddal Nordine <nordine.feddal.etu@univ-lille.fr>
2021-08-02 16:29:02 +0200

    ROK: FEAT added a page for API connection error

---
+++ src/components/apiErrConnection.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function ApiErrConnection() {
+  return (
+    <div>
+      <h2>
+        Sorry for the inconvenience. We are experiencing connection problems
+        with our server.
+      </h2>
+    </div>
+  );
+}
