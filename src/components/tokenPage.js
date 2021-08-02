fe69b8e4956f3174438add1c148b0e85f24617e6
Feddal Nordine <nordine.feddal.etu@univ-lille.fr>
2021-08-02 16:26:11 +0200

    ROK: REFACTOR removed comment

--- src/components/tokenPage.js
+++ src/components/tokenPage.js
@@ -1,26 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
+
 export default function TokenPage(props) {
   return (
-    /*
-    <div>
-      <h2>You just registered the following Simulation.</h2>
-      <div>
-        <p className="lead">
-          Username: {props.location.state.username} <br />
-          Model: {props.location.state.model} <br />
-          Progress: {props.location.state.progress} <br />
-        </p>
-        <div>
-          <p className="lead">
-            Please keep theses informations safe, you will need theses
-            informations to update or delete your simulation.
-          </p>
-          <h1>Token : {props.location.state.token}</h1>
-          <h1>id : {props.location.state.id}</h1>
-        </div>
-      </div>
-    </div>*/
     <div>
       <h2>You just registered the following Simulation.</h2>
       <div>
